fb39ff726adfa841e0ad1793225340456761b1d6
cheskel <twerskycheskel@gmail.com>
2021-04-29 01:22:02 +0300

    v 0.8.0 added option to pass url which returns JSON fixed test script added -o option to cli which will output the edited JSON the stdout

--- jsoneditor/jsoneditor.py
+++ jsoneditor/jsoneditor.py
@@ -8,7 +8,8 @@ import random
 import os
 import sys
 import platform
-
+import re
+import requests
 
 
 if platform.system() == 'Darwin':
@@ -26,7 +27,19 @@ os.environ['WERKZEUG_RUN_MAIN'] = 'true'
 
 def editjson(data: Union[dict, str], finnish_callback: callable = None, options: dict = None):
     if type(data) is str:
-        data = json.loads(data)
+        try:
+            data = json.loads(data)
+        except ValueError:
+            regex_pattern = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+            url = re.findall(regex_pattern, data)
+            if url:
+                text_data = requests.get(url[0]).text
+                try:
+                    data = json.loads(text_data)
+                except ValueError:
+                    raise ValueError('The url passed did not return valid JSON')
+            else:
+                raise ValueError('No valid value passed')
 
     app = Flask('jsoneditor', template_folder= install_dir + '/templates')
     port = random.randint(1023, 65353)
@@ -39,7 +52,7 @@ def editjson(data: Union[dict, str], finnish_callback: callable = None, options:
         @app.route('/post', methods=['POST'])
         def callback_route():
             data = request.json['data']
-            finnish_callback(data)
+            finnish_callback(json.dumps(data))
             return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
     
     server = Process(target=app.run, args=('localhost', port))
@@ -47,12 +60,17 @@ def editjson(data: Union[dict, str], finnish_callback: callable = None, options:
     webbrowser.open(f'http://localhost:{port}/')
 
 def main():
+    if '-o' in sys.argv:
+        sys.argv.remove('-o')
+        callback = print
+    else:
+        callback = None
     if not os.isatty(0):
         data = ''.join(x for x in sys.stdin)
     else:
         if len(sys.argv) == 2:
-            data = sys.argv[1]
+            data = sys.argv[1]            
         else:
             raise Exception('Got invalid number of arguments')
 
-    editjson(data)
+    editjson(data= data, finnish_callback= callback)
--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,22 @@
 import sys
-sys.path.append('../jsoneditor')
+import os
+
+sys.path.insert(0, '/'.join(os.path.dirname(os.path.realpath(__file__)).split('/')[:-1]) + '/jsoneditor')
 
 import jsoneditor
 import requests
 
+
+# Test dict
 jsoneditor.editjson(requests.get('https://jsonplaceholder.typicode.com/posts').json())
+
+
+# Test string
 jsoneditor.editjson(requests.get('https://jsonplaceholder.typicode.com/comments').text)
 
-
-# Test editing
+#Test editing
 jsoneditor.editjson({'hi': '#466'}, lambda data: print(data), {'colorPicker': True})
+
+
+# Test urls
+jsoneditor.editjson('https://jsonplaceholder.typicode.com/users')
