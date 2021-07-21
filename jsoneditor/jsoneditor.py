3195a05dd97d350a74fd3d152a6ec3f9ee8927ae
Cheskel Twersky <twerskycheskel@gmail.com>
2021-07-21 16:14:59 +0300

    Minor improvements - convert mapping to dict - remove re for detecting url - print url when browser is not available

--- jsoneditor/jsoneditor.py
+++ jsoneditor/jsoneditor.py
@@ -4,11 +4,12 @@ import webbrowser
 import random
 import os
 import sys
-import re
 import requests
 import mimetypes
 import argparse
 from typing import Union
+from collections.abc import Mapping
+from urllib.parse import urlparse
 from wsgiref.simple_server import make_server, WSGIRequestHandler
 
 
@@ -48,19 +49,25 @@ class Server:
             try:
                 data = json.loads(data)
             except ValueError:
-                regex_pattern = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
-                url = re.findall(regex_pattern, data)
-                if url:
-                    text_data = requests.get(url[0]).text
+                try:
+                    result = urlparse(data)
+                    is_url = all([result.scheme, result.netloc])
+                except:
+                    is_url = False
+                if os.path.exists(data):
+                    with open(data, 'r') as f:
+                        data = json.load(f)
+                elif is_url:
+                    text_data = requests.get(data).text
                     try:
                         data = json.loads(text_data)
                     except ValueError:
                         raise ValueError('The url passed did not return valid JSON')
-                elif os.path.exists(data):
-                    with open(data, 'r') as f:
-                        data = json.load(f)
                 else:
                     raise ValueError('No valid value passed')
+        elif isinstance(data, Mapping):
+            # convert to dict as some mappings are not json serializable
+            data = dict(data)
         return data
 
 
@@ -118,7 +125,7 @@ class Server:
         server.run_in_background = self.run_in_background
 
         if not self.run_in_background:
-            webbrowser.open(f'http://localhost:{self.port}/')
+            open_browser(self.port)
         
         server.serve_forever()
 
@@ -130,10 +137,15 @@ def editjson(data: Union[dict, str], callback: callable = None, options: dict = 
     if server.run_in_background:
         thread = threading.Thread(target=server.start)
         thread.start()
-        webbrowser.open(f'http://localhost:{server.port}/')
+        open_browser(server.port)
     else:
         server.start()
 
+
+def open_browser(port: int) -> None:
+    browser_opened = webbrowser.open(f'http://localhost:{port}/')
+    if not browser_opened:
+        print(f"couldn't launch brower, Please open this link to see the JSON: http://localhost:{port}/")
 
 
 # cli function
